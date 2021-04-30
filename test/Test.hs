2b86f4a48fc1545fac2f121a1ac562246926e7a6
Kwasniok <Kwasniok@users.noreply.github.com>
2021-04-30 21:41:58 +0200

    add test

--- test/Test.hs
+++ test/Test.hs
@@ -1,6 +1,24 @@
 module Main (main) where
 
-import Data.FA.DFA
+import Prelude
+import System.Exit (exitFailure, exitSuccess)
+
+import Data.Set
+import Data.FA.DFA as DFA
+
+qs = fromList [0,1]
+as = fromList ['a','b']
+tf q a = 1 - q
+qi = 0
+qfs = fromList [1]
+dfa = DFA.DFA qs as tf qi qfs
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = if
+    (DFA.read dfa "" == 0) &&
+    (DFA.read dfa "a" == 1) &&
+    (DFA.read dfa "ab" == 0) &&
+    (DFA.accepts dfa "" == False) &&
+    (DFA.accepts dfa "a" == True) &&
+    (DFA.accepts dfa "ab" == False)
+    then exitSuccess else exitFailure
