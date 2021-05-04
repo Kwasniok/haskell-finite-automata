3472831b023f25b3aedf4ebc3803ee34b932d99b
Kwasniok <Kwasniok@users.noreply.github.com>
2021-05-04 16:37:27 +0200

    add DFA to string

--- src-lib/Data/FA/DFA.hs
+++ src-lib/Data/FA/DFA.hs
@@ -2,10 +2,12 @@ module Data.FA.DFA (
     DFA (DFA),
     read,
     accepts,
+    showTransferRules,
 ) where
 
 import qualified Prelude
 import qualified Data.Set as Set
+import qualified Data.List as List
 
 data DFA q a = DFA {
     states :: Set.Set q,
@@ -24,3 +26,11 @@ read fa (w:ws) = read (readSymbol fa w) ws
 
 accepts :: (Prelude.Ord q) => DFA q a -> [a] -> Prelude.Bool
 accepts fa ws = Set.member (read fa ws) (acceptStates fa)
+
+showTransferRules :: (Prelude.Ord q, Prelude.Show q, Prelude.Ord a, Prelude.Show a) => DFA q a -> Prelude.String
+showTransferRules (DFA qs as tf qi qfs) = rules
+    where
+        rules = (List.intercalate ", " (Prelude.map showRule (Set.elems edgeTriples)))
+        edgeTriples = Set.map makeEdgeTriple (Set.cartesianProduct qs as)
+        showRule (q1, q2, a) = "(" List.++ (Prelude.show q1) List.++ "->" List.++ (Prelude.show q2) List.++ ")->" List.++  (Prelude.show a)
+        makeEdgeTriple (q, a) = (q, tf q a, a)
