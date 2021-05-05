c231aa614eabb78d0a3e11c5b40507f2a1baab79
Kwasniok <Kwasniok@users.noreply.github.com>
2021-05-05 17:27:07 +0200

    add DFA to NFA

--- src-lib/Data/FA.hs
+++ src-lib/Data/FA.hs
@@ -5,11 +5,28 @@ State,
     accepts,
     DFA (MkDFA),
     NFA (MkNFA),
+    dfaToNfa,
 ) where
 
+import Data.Maybe
+import Data.Set as Set
 import Data.Collection.Finite
 import Data.FA.State
 import Data.FA.Symbol
 import Data.FA.Base (FA, accepts)
 import Data.FA.DFA (DFA (MkDFA))
 import Data.FA.NFA (NFA (MkNFA))
+
+-- transforms f :: (a -> b) to h ::(Maybe a -> c)
+-- with a transformation g :: (b -> c) and a default value d ::c
+-- such that:
+-- h Nothing = d :: c)
+-- h (Just x) = g (f x)
+maybefy :: (a -> b) -> (b -> c) -> c -> (Maybe a -> c)
+maybefy _ _ d Nothing = d
+maybefy f g _ (Just x) = g (f x)
+
+-- converts a DFA to an equivalent NFA
+dfaToNfa :: (State a, Symbol b, Ord b) => DFA a b -> NFA a b
+dfaToNfa (MkDFA t1 qi qas) = (MkNFA t2 qi qas) where
+    t2 q = maybefy (t1 q) Set.singleton Set.empty
