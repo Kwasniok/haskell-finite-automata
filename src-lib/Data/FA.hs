a948859f41f08d846d4599e63b8577a933f3ef68
Kwasniok <Kwasniok@users.noreply.github.com>
2021-05-06 18:06:25 +0200

    add NFA to DFA conversion

--- src-lib/Data/FA.hs
+++ src-lib/Data/FA.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Data.FA (
 State,
     Symbol,
@@ -7,6 +9,7 @@ State,
     DFA (MkDFA),
     NFA (MkNFA),
     dfaToNfa,
+    nfaToDfa,
 ) where
 
 import Prelude hiding (map, filter)
@@ -34,3 +37,16 @@ maybefy f g _ (Just x) = g (f x)
 dfaToNfa :: (State a, Symbol b, Ord b) => DFA a b -> NFA a b
 dfaToNfa (MkDFA t1 qi qas) = (MkNFA t2 qi qas) where
     t2 q = maybefy (t1 q) singleton Empty
+
+-- converts an NFA to an equivalent DFA via the power automaton
+nfaToDfa :: forall a b. (State a, Symbol b, Ord a) => NFA a b -> DFA (FiniteSet a) b
+nfaToDfa nfa@(MkNFA t1 qis1 qas1) = MkDFA t2 qis2 qas2 where
+    t2 :: FiniteSet a -> b -> FiniteSet a
+    t2 qs a = readSymbol nfa qs a
+    qis2 :: FiniteSet a
+    qis2 = singleton qis1
+    qas2 :: FiniteSet (FiniteSet a)
+    qas2 = filter anyAccepting $ fromList (states :: [FiniteSet a])
+    -- True if any of the states is an accepting state
+    -- FiniteSet a -> Bool
+    anyAccepting = (any (\q -> elementOf q qas1)) . toList
